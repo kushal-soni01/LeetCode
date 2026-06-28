1694beee33bf653f24a1121e3b1a3a2e22ef9523
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-06-28 23:45:35 +0530

    Submitted LC-Q1846 with Time Complexity: O(n log n) by Greedy Algorithm with Sorting approach.

--- 1846-Maximum-Element-After-Decreasing-and-Rearranging/solution.java
+++ 1846-Maximum-Element-After-Decreasing-and-Rearranging/solution.java
@@ -4,7 +4,7 @@ class Solution {
         int n = arr.length;
         arr[0] = 1;
         for(int i=1; i<n; i++){
-            arr[i] = arr[i] - arr[i-1] > 1 ? arr[i-1]+1 : arr[i];
+            arr[i] = Math.min(arr[i], arr[i-1]+1);
         }
         return arr[n-1];
     }
