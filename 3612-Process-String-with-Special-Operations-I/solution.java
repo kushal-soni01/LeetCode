a2a9b32b99695a48a83f175c0d96e3f114480720
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-06-29 19:23:31 +0530

    Submitted LC-Q3612 with Time Complexity: O(n * m) by Simulation with StringBuilder approach.

---
+++ 3612-Process-String-with-Special-Operations-I/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+        for(char c: s.toCharArray()){
+            if(c == '*'){
+                int len = sb.length();
+                if(len>0){
+                    sb.deleteCharAt(len-1);
+                }
+            }
+            else if(c == '#'){
+                sb.append(sb);
+            }
+            else if(c == '%'){
+                sb.reverse();
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
