0246bd3d3512a6cf23fbc9d223255a144083ab3c
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-06-29 19:49:23 +0530

    Submitted LC-Q3737 with Time Complexity: O(n²) by Prefix Sum with Nested Loop approach.

---
+++ 3737-Count-Subarrays-With-Majority-Element-I/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int sum = 0, n = nums.length, ans = 0;
+        for(int i=0; i<n; i++){
+            sum += (nums[i] == target) ? 1 : -1;
+            nums[i] = sum;
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]>0) ans++;
+            for(int j=i; j<n; j++){ 
+                if(nums[j] - nums[i] > 0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
