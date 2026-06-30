e3bdda62ecae3e725241bdf415bb43b5c0ab99d8
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-06-30 16:38:28 +0530

    Submitted LC-Q1394 with Time Complexity: O(n) by Single Pass Hash Map approach.

---
+++ 1394-Find-Lucky-Integer-in-an-Array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        int ans = -1;
+        for(int i: map.keySet()){
+            if(i==map.get(i)) ans = Math.max(i, ans);
+        }
+        return ans;
+    }
+}
