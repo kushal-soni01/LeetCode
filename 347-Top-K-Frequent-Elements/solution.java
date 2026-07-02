d24ed0b9c7eef143610620c233c19d9f8dede22a
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-07-03 01:26:59 +0530

    Submitted LC-Q347 with Time Complexity: O(n log k) by Priority Queue with Hash Map approach.

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(k, (a,b) -> Integer.compare(b[0], a[0]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
         HashMap<Integer, Integer> freqMap = new HashMap<>();
         for(int num: nums){
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
         for(int key: freqMap.keySet()){
-            maxHeap.offer(new int[]{freqMap.get(key), key});
+            minHeap.offer(new int[]{freqMap.get(key), key});
+            if(minHeap.size()>k) minHeap.poll();
         }
         int ans[] = new int[k];
         for(int i=0; i<k; i++){
-            ans[i] = maxHeap.poll()[1];
+            ans[i] = minHeap.poll()[1];
         }
         return ans;
     }
