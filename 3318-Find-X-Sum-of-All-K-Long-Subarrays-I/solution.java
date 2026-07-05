c294a236475d97b6db72a20beef5794dd6e3f755
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-07-05 23:49:47 +0530

    Submitted LC-Q3318 with Time Complexity: O(n * k * log x) by Sliding Window with Hash Map and Priority Queue approach.

---
+++ 3318-Find-X-Sum-of-All-K-Long-Subarrays-I/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<k-1; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        for(int i=k-1; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            PriorityQueue<int[]> kMaxHeap = new PriorityQueue<>((a, b) ->{
+                return a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]);
+            }); //[occurances, value]
+            for(int num: map.keySet()){
+                kMaxHeap.offer(new int[] {map.get(num), num});
+                if(kMaxHeap.size()>x) kMaxHeap.poll();
+            }
+            int ele = 0, sum = 0;
+            while(ele<x && !kMaxHeap.isEmpty()){
+                int[] data = kMaxHeap.poll();
+                sum+=data[0]*data[1];
+            }
+            ans[i-k+1] = sum;
+            int start = nums[i-k+1];
+            if(map.get(start) == 1) map.remove(start);
+            else map.put(start, map.get(start)-1);
+        }
+        return ans;
+    }
+}
