24a8be3f5cda9d15cddbb30d63eb3c2077073dde
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-07-18 13:41:25 +0530

    Submitted LC-Q334 with Time Complexity: O(n) by Greedy algorithm with two variables tracking smallest and second smallest values approach.

---
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int smallest = Integer.MAX_VALUE, smaller = Integer.MAX_VALUE;
+        for(int num: nums){
+            if(num <= smallest) smallest = num;
+            else if (num <= smaller) smaller = num;
+            else return true;
+        }
+        return false;
+    }
+}
