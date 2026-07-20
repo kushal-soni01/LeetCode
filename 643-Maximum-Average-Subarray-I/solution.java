50624e95073f147695372430c757e9faca3ae688
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-07-20 16:06:44 +0530

    Submitted LC-Q643 with Time Complexity: O(n) by Sliding Window approach.

---
+++ 643-Maximum-Average-Subarray-I/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum=0;
+        for(int i=0; i<k; i++){
+            sum+=nums[i];
+        }
+        double ans = sum/k;
+        for(int i=k; i<nums.length; i++){
+            sum+=nums[i]-nums[i-k];
+            ans = Math.max(sum/k, ans);
+        }
+        return ans;
+    }
+}
