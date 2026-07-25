5d5dd75760d93ed946fa6a4562baf674a6f377bc
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-07-25 12:30:58 +0530

    Submitted LC-Q2390 with Time Complexity: O(n) by Stack-based simulation approach.

---
+++ 2390-Removing-Stars-From-a-String/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(!stack.isEmpty() && c == '*') stack.pop();
+            else stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c: stack) sb.append(c);
+        return sb.toString();
+    }
+}
