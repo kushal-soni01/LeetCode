c4437b8caf9bbc181e0df8088a7619df49c317d1
Kushal Soni <Kushalsoni.ec@gmail.com>
2026-08-23 16:07:58 +0530

    Submitted LC-Q547 with Time Complexity: O(n^2) by Depth-First Search (DFS) traversal approach.

--- 547-Number-of-Provinces/solution.java
+++ 547-Number-of-Provinces/solution.java
@@ -3,9 +3,10 @@ class Solution {
         if(visited[city]) return;
         visited[city] = true;
         for(int i=0; i<n; i++){
-            if(isConnected[city][i] == 1) dfs(isConnected, visited, i, n);
+            if(!visited[i] && isConnected[city][i] == 1) dfs(isConnected, visited, i, n);
         }
     }
+
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length, provinces = 0;
         boolean visited[] = new boolean[n];
@@ -15,7 +16,7 @@ class Solution {
             dfs(isConnected, visited, i, n);
             provinces++;
         }
-        
+
         return provinces;
     }
 }
